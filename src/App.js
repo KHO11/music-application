4fc522a4b95a93a295e07b9458383586801ab838
KHO11 <54800683+KHO11@users.noreply.github.com>
2021-07-30 15:01:54 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ import Player from "./Player";
 function App() {
   const [songs] = useState([
     {
-      title: "Forget me too ft. Halsey",
-      artist: "Machine Gun Kelly",
+      title: "Bleach Number One",
+      artist: " Shirō Sagisu",
       img_src: "./images/song-1.jpg",
-      src: "./Music/public_music_on-n-on.mp3"
+      src: "./Music/[MP3DOWNLOAD.TO] Bleach - Ichigo's Theme - Number One-320k"
     },
     {
-      title: "Song 2",
-      artist: "Artist 2",
+      title: "Melodic Dreams",
+      artist: "Whitesand",
       img_src: "./images/song-2.jpg",
-      src: "./Music/public_music_somebody-new.mp3"
+      src: "./Music/Whitesand - Melodic Dreams"
     },
     {
-      title: "Song 3",
-      artist: "Artist 3",
+      title: "Bleach Rolling Star",
+      artist: "Yui",
       img_src: "./images/song-3.jpg",
-      src: "./Music/public_music_on-n-on.mp3"
+      src: "./Music/bleach-soundtrack_bleach-op5-rolling-star-by-yui"
     },
     {
-      title: "Song 4",
-      artist: "Artist 4",
+      title: "Bleach Memories of nobody",
+      artist: "Shiro Sagisu",
       img_src: "./images/song-4.jpg",
-      src: "./Music/public_music_somebody-new.mp3"
+      src: "./Music/bleach-soundtrack_memories-of-nobody-ed-mp3"
     }
   ]);
 
